ec33c59479422639145697b96750101efaa8275b
JasonKim0122 <JasonKim0122@gmail.com>
2022-11-02 20:16:19 -0500

    added styles for navbar

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -3,6 +3,9 @@ import styles from "../styles/Navbar.module.css"
 const Navbar = () => {
     return (
         <div className ={styles.container}>
+        <div className ={styles.item}></div>
+        <div className ={styles.item}></div>
+        <div className ={styles.item}></div>
             navbar
         </div>
     )

d172cfc010175f187240240e5581aea9d8bfbc2b
JasonKim0122 <JasonKim0122@gmail.com>
2023-05-17 15:47:24 -0500

    created footer and added css

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,27 @@
+import Image from 'next/image';
+import styles from '../styles/Footer.module.css';
+
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <Image src='/img/menu.JPG' style={'cover'} alt='' fill/>
+            </div>
+            <div className={styles.item}>
+                <div className={styles.card}>
+                    <h2 className={styles.motto}>
+                        VIET FOOD HERE VIET FOOD HERE
+                    </h2>
+                </div>
+                <div className={styles.card}>
+                    <h1 className={styles.title}>Contact us here!</h1>
+                    <p className={styles.text}>222-222-2222</p>
+                </div>
+                <div className={styles.card}>
+                    <h1 className={styles.title}>Working Hours</h1>
+                    <p className={styles.text}>TBD</p>
+                </div>
+            </div>
         </div>
     )
 }

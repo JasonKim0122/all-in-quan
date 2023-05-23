003827e25c2f14781b62b20deebb65cafa552711
JasonKim0122 <JasonKim0122@gmail.com>
2023-05-22 19:08:37 -0500

    created new useState to change the prices of certain products

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react';
 import styles from '../../styles/Product.module.css';
 import Image from "next/legacy/image";
+import axios from 'axios';
 
-const Product = () => {
+const Product = ({food}) => {
+    const [price, setPrice] = useState(food.prices[0]);
     const [size, setSize] = useState(0);
-    const food = {
-        id: 1,
-        img: '/img/food-1.JPG',
-        name: 'Egg rolls',
-        price: [5, 10, 15],
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi repellendus officiis aliquid vitae id rerum fuga, nulla sint consectetur, ipsa non velit saepe delectus veritatis dolorum voluptatem, corrupti voluptatum.'
-    }
+    const [quantity, setQuantity] = useState(1);
+
+    const changePrice = (number) => {
+        setPrice(price + number);
+    };
+
+    const handleSize = (sizeIndex) => {
+        const difference = food.prices[sizeIndex] - food.prices[size];
+        setSize(sizeIndex);
+        changePrice(difference)
+    };
 
     return <div className={styles.container}>
         <div className={styles.left}>
@@ -19,30 +25,39 @@ const Product = () => {
             </div>
         </div>
         <div className={styles.right}>
-            <h1 className={styles.title}>{food.name}</h1>
-            <span className={styles.price}>${food.price[size]}</span>
+            <h1 className={styles.title}>{food.title}</h1>
+            <span className={styles.price}>${price}</span>
             <p className={styles.description}>{food.description}</p>
             <h3 className={styles.choose}>Please Choose the size</h3>
             <div className={styles.sizes}>
-                <div className={styles.size} onClick={() => setSize(0)}>
+                <div className={styles.size} onClick={() => handleSize(0)}>
                     <Image src='/img/size.PNG' layout="fill" alt='' />
                     <span className={styles.number}>Small</span>
                 </div>
-                <div className={styles.size} onClick={() => setSize(1)}>
+                <div className={styles.size} onClick={() => handleSize(1)}>
                     <Image src='/img/size.PNG' layout="fill" alt='' />
                     <span className={styles.number}>Medium</span>
                 </div>
-                <div className={styles.size} onClick={() => setSize(2)}>
+                <div className={styles.size} onClick={() => handleSize(2)}>
                     <Image src='/img/size.PNG' layout="fill" alt='' />
                     <span className={styles.number}>Large</span>
                 </div>
             </div>
             <div className={styles.add}>
-                <input type='number' defaultValue={1} className={styles.quantity}/>
+                <input onChange={(e) => setQuantity(e.target.value)} type='number' defaultValue={1} className={styles.quantity}/>
                 <button className={styles.button}>Add to Cart!</button>
             </div>
         </div>
     </div>
-}
+};
+
+export const getServerSideProps = async ({params}) => {
+    const res = await axios.get(`http://localhost:3000/api/products/${params.id}`);
+    return {
+     props: {
+       food: res.data,
+     }
+    }
+   };
 
 export default Product;

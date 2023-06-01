b4295662935d9c485c28336e43aa3f4aed2ad65c
JasonKim0122 <JasonKim0122@gmail.com>
2023-06-01 14:25:41 -0500

    created handleStatus function to change current status of pending orders

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 const Index = ({orders, products}) => {
     const [foodList, setFoodList] = useState(products);
     const [orderList, setOrderList] = useState(orders);
+    const status = ["Preparing", "Ready for pickup"];
 
     const handleDelete = async (id) => {
         try {
@@ -15,6 +16,23 @@ const Index = ({orders, products}) => {
             console.log(err)
         }
     }
+
+    const handleStatus = async (id) => {
+
+        const item = orderList.filter(order => order._id === id)[0]
+        const currentStatus = item.status
+
+        try {
+            const res = await axios.put('http://localhost:3000/api/orders/' + id, {status: currentStatus + 1 });
+            setOrderList([
+                res.data,
+                ...orderList.filter(order => order._id !== id),
+            ]);
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.item}>
@@ -67,18 +85,20 @@ const Index = ({orders, products}) => {
                             <th>Action</th>
                         </tr>
                     </tbody>
-                    <tbody>
+                    {orderList.map((order) => (
+                        <tbody key ={order._id}>
                         <tr className={styles.trTitle}>
-                            <td>{"123123123123".slice(0,5)}...</td>
-                            <td>John</td>
-                            <td>$50</td>
-                            <td>Paid</td>
-                            <td>Preparing</td>
+                            <td>{order._id.slice(0,5)}...</td>
+                            <td>{order.customer}</td>
+                            <td>${order.total}</td>
+                            <td>{order.method === 0 ? (<span>Cash</span>) : (<span>Paid</span>)}</td>
+                            <td>{status[order.status]}</td>
                             <td>
-                                <button >Next Stage</button>
+                                <button onClick={() => handleStatus(order._id)}>Next Stage</button>
                             </td>
                         </tr>
                     </tbody>
+                    ))}
                 </table>
             </div>
         </div>
